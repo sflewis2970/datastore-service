9b5de1c1bc80b9cba6317ad8bd5e8669680ca020
Shawn F Lewis <44281309+sflewis2970@users.noreply.github.com>
2022-06-21 14:59:12 -0700

    Update controllers.go
    * Updated controller initialization process

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,14 +1,30 @@
 package controllers
 
 import (
+	"log"
 	"sync"
 
 	"github.com/sflewis2970/datastore-service/config"
 )
 
-var dbMutex sync.Mutex
-var cfgData *config.ConfigData
+type controller struct {
+	dbMutex sync.Mutex
+	cfgData *config.ConfigData
+}
 
-func LoadConfig() {
-	cfgData = config.GetConfig().GetConfigData()
+var ctrlr *controller
+
+func InitializeController() {
+	if ctrlr == nil {
+		ctrlr = new(controller)
+
+		// Get config object
+		cfg, getErr := config.GetConfig()
+		if getErr != nil {
+			log.Fatal("Error getting config")
+		}
+
+		// Load config data into property
+		ctrlr.cfgData = cfg.GetConfigData()
+	}
 }
