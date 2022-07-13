babb993789e2a92f965c686355da1e97fbcd7ab9
Shawn F Lewis <44281309+sflewis2970@users.noreply.github.com>
2022-07-12 22:54:46 -0700

    Updated config files

--- models/goredis/goredis.go
+++ models/goredis/goredis.go
@@ -157,6 +157,8 @@ func GetRedisModel(cfgData *config.ConfigData) *dbModel {
 	// Once the external packages access the values, the environment has lready been taken
 	// care of.
 	addr := redisModel.cfgData.Redis.URL + redisModel.cfgData.Redis.Port
+	log.Print("redis URL...: ", redisModel.cfgData.Redis.URL)
+	log.Print("redis Port...: ", redisModel.cfgData.Redis.Port)
 	log.Print("The redis address is...: ", addr)
 
 	redisOptions = &redis.Options{
