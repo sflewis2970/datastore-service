9deb589980bf72b6c1ee19934b4c31549a5956e0
Shawn F Lewis <44281309+sflewis2970@users.noreply.github.com>
2022-07-13 14:22:30 -0700

    Update config.go
    Removed debug messages

--- config/config.go
+++ config/config.go
@@ -202,9 +202,7 @@ func (c *config) getConfigEnv() error {
 			log.Print("Redis Port: ", c.cfgData.Redis.Port)
 
 			// redis Password
-			// var passwdSet bool
 			c.cfgData.Redis.Password, _ = redisURL.User.Password()
-			log.Print("Password: ", c.cfgData.Redis.Password)
 		} else {
 			c.cfgData.Redis.URL = os.Getenv(REDIS_URL)
 			c.cfgData.Redis.Port = os.Getenv(REDIS_PORT)
